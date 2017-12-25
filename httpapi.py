00d5432d7c12b66ec33503b5a22a61f68d9706e3
daveqin <1029329095@qq.com>
2017-12-25 22:51:25 +0800

    修复者httpapit的bug问题

--- httpapi.py
+++ httpapi.py
@@ -7,6 +7,9 @@ from stockapi import *
 
 
 
+        
+        
+
 # 定义函数，参数是函数的两个参数，都是python本身定义的，默认就行了。
 def application(environ, start_response):
     # 定义文件请求的类型和当前请求成功的code
@@ -21,26 +24,27 @@ def application(environ, start_response):
     action = params.get('action', [''])[0]
     stock = params.get('stock', [''])[0]
     
-    callbacks= params.get('callbacks', [''])[0]
-    print callbacks
+    jsoncallback= params.get('jsoncallback', [''])[0]
+    #print callbacks
     
     if action=='get_hist':
          
           #print get_hist(stock)
           dic=get_hist(stock)
           
-          dic= {'callbacks': callbacks,'action':action}
-          return callbacks+"("+json.dumps(dic)+")"
+          #dic= {'callbacks': callbacks,'action':action}
+          return jsoncallback+"("+dic+")"
     
     # 组成一个数组，数组中只有一个字典
     dic = {'name': name, 'no': no,'action':action}
     
     #$_GET['jsoncallback'] . "(".json_encode($array_e).")";
-    return [json.dumps(dic)]
+    print "xxx("+json.dumps(dic)+")"
+    return jsoncallback+"("+json.dumps(dic)+")"
 
 
 if __name__ == "__main__":
-   port = 6126
+   port = 6276
    httpd = make_server("0.0.0.0", port, application)
    print "serving http on port {0}...".format(str(port))
    httpd.serve_forever()
--- stockapi.py
+++ stockapi.py
@@ -13,16 +13,24 @@ import matplotlib.pyplot as plt
 
 
 
-
-
+class stockapi:
+    def __init__(self,stock ):
+        self.stock = stock
+    
+    def get_hist(self):
+        
+        data=ts.get_hist_data(self.stock)
+        data1=data[['open','high','close','low']]
+        
+        return data1.to_json(orient='split')
+        
+    def get_mac(self):
+        
 #print data1.to_json(orient='split')
 
-def get_hist(stock):
-    
-    data=ts.get_hist_data(stock)
-    data1=data[['open','high','close','low']]
-    
-    return data1.to_json(orient='split')
+
+st = stockapi("002486")
+print st.get_hist()
 
 
 #print get_hist('002486')
